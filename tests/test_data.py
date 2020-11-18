4b933f8c9b62bfb76caf5672b80a769f7d70652d
Stephanie Kirmer <s.kirmer@gmail.com>
2020-11-18 10:01:46 -0600

    Forgot to change get to list

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,7 +6,7 @@ from dask_pytorch.data import S3ImageFolder
 
 def test_image_folder_constructor():
     fake_file_list = ["d/a.jpg", "c/b.jpg"]
-    with patch("dask_pytorch.data._get_all_files", return_value=fake_file_list):
+    with patch("dask_pytorch.data._list_all_files", return_value=fake_file_list):
         fake_transform = Mock()
         fake_target_transform = Mock()
         folder = S3ImageFolder(
@@ -21,14 +21,14 @@ def test_image_folder_constructor():
 
 def test_image_folder_len():
     fake_file_list = ["d/a.jpg", "c/b.jpg"]
-    with patch("dask_pytorch.data._get_all_files", return_value=fake_file_list):
+    with patch("dask_pytorch.data._list_all_files", return_value=fake_file_list):
         folder = S3ImageFolder("fake-bucket", "fake-prefix/fake-prefix")
     assert len(folder) == 2
 
 
 def test_image_folder_getitem():
     fake_file_list = ["d/a.jpg", "c/b.jpg"]
-    with patch("dask_pytorch.data._get_all_files", return_value=fake_file_list):
+    with patch("dask_pytorch.data._list_all_files", return_value=fake_file_list):
         folder = S3ImageFolder("fake-bucket", "fake-prefix/fake-prefix")
     with patch("dask_pytorch.data._read_s3_fileobj") as read_s3_fileobj, patch(
         "dask_pytorch.data._load_image_obj"
